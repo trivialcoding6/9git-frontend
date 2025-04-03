97b1518d09af4765f5948aa2e1990b38f6f49b84
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-03 20:43:04 +0900

    ✨ [SCRUM-79]-main-Card-컴포넌트-제작

--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "@/components/common/Logo";
 import { Header } from "@/components/shared/Header";
+import Card from "@/components/common/Card"
 
 export const Home = () => {
   return (
@@ -7,6 +8,10 @@ export const Home = () => {
       <Header>
         <Logo width={45} height={45} />
       </Header>
+
+       <div className="mt-6 flex flex-col items-center gap-4">
+        <Card title="목표 진행률" isMore height="h-40" />
+      </div>
     </>
   );
 };
