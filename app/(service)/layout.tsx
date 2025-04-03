d468997875afa77c5b5c149ddb870e4c0f179655
Eatcoding <trivialcoding6@gmail.com>
2025-04-03 19:57:39 +0900

    ✨ [SCRUM-96]-FE-Base-모달-팝업-제작

--- app/(service)/layout.tsx
+++ app/(service)/layout.tsx
@@ -1,10 +1,11 @@
 import { BottomTabs } from "@/components/shared/BottomTabs/BottomTabs";
-
+import { Modal } from "@/components/shared/Modal/Modal";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
       <main className="flex-1 overflow-auto scrollbar-hide">{children}</main>
       <BottomTabs />
+      <Modal />
     </div>
   );
 }
---
+++ components/service/modals/ProfileModal.tsx
@@ -0,0 +1,3 @@
+export default function ProfileModal() {
+  return <div>ProfileModal</div>;
+}
