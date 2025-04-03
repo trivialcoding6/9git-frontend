3b90bcb865f7d0bda598dd43ab6926aed34e96d5
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-03 23:39:06 +0900

    [SCRUM-80] ProgressBar 스타일 수정 및 Home 적용

--- components/common/progressbar.tsx
+++ components/common/progressbar.tsx
@@ -1,20 +1,22 @@
 import { Progress } from "@/components/ui/progress"
 
-
 interface ProgressBarProps {
   value: number
   className?: string
   indicatorClassName?: string
 }
 
-export default function ProgressBar({value,className,indicatorClassName}:ProgressBarProps){
-    return(
-        <div>
-            <div className="text-xs text-right text-[#79522F] font-semibold">{value}% 달성</div>
-            <Progress value={value}
-            className={className}
-            indicatorClassName={indicatorClassName} 
-                />
-        </div>
-    )
+export const ProgressBar = ({ value, className, indicatorClassName }: ProgressBarProps) => {
+  return (
+    <div>
+      <div className="text-xs text-right text-[#79522F] font-semibold">
+        {value}% 달성
+      </div>
+      <Progress
+        value={value}
+        className={className}
+        indicatorClassName={indicatorClassName}
+      />
+    </div>
+  )
 }
--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -1,17 +1,24 @@
 import { Logo } from '@/components/common/Logo';
 import { Header } from '@/components/shared/Header';
 import Card from '@/components/common/Card';
+import { ProgressBar } from '@/components/common/progressbar';
 
 export const Home = () => {
   return (
-    <>
+    <div>
       <Header>
         <Logo width={45} height={45} />
       </Header>
 
       <div className="mt-6 flex flex-col items-center gap-4">
         <Card title="목표 진행률" isMore height="h-40" />
+
+        <ProgressBar
+          value={75}
+          className="bg-[#F4DFC2]" // 전체 바 배경
+          indicatorClassName="bg-[#FBAA24]" // 진행된 부분 색상
+        />
       </div>
-    </>
+    </div>
   );
 };
