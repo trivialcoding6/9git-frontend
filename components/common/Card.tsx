8a1c357eb01e66c57dd7cea95f7f54fc514794a2
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-04 10:00:17 +0900

    [SCRUM-79] Prettier 설정 적용 및 코드 정리

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,29 +1,26 @@
 // 목표 진행률
-import {Separator} from "@/components/ui/separator"
+import { Separator } from '@/components/ui/separator';
 
 interface CardProps {
-  title: string
-  height?: string
-  isMore?: boolean
+  title: string;
+  height?: string;
+  isMore?: boolean;
 }
 
 export default function Card({
   title,
   height, //높이 수정
-  isMore= false,
-
+  isMore = false,
 }: CardProps) {
   return (
     <section className={`bg-[#fff0d5] rounded-xl p-4 space-y-2 w-[90%] ${height}`}>
       <div className="flex justify-between items-center font-semibold">
         <span>{title}</span>
-        {isMore && (<div className="text-sm text-[#7F4E28] font-semibold hover:text-[#A35F30]">
-
-
-      + 더보기
-    </div>)}
+        {isMore && (
+          <div className="text-sm text-[#7F4E28] font-semibold hover:text-[#A35F30]">+ 더보기</div>
+        )}
       </div>
       <Separator className="bg-[#f4ddbd]" />
     </section>
-  )
+  );
 }
--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -1,6 +1,6 @@
-import { Logo } from "@/components/common/Logo";
-import { Header } from "@/components/shared/Header";
-import Card from "@/components/common/Card"
+import { Logo } from '@/components/common/Logo';
+import { Header } from '@/components/shared/Header';
+import Card from '@/components/common/Card';
 
 export const Home = () => {
   return (
@@ -9,7 +9,7 @@ export const Home = () => {
         <Logo width={45} height={45} />
       </Header>
 
-       <div className="mt-6 flex flex-col items-center gap-4">
+      <div className="mt-6 flex flex-col items-center gap-4">
         <Card title="목표 진행률" isMore height="h-40" />
       </div>
     </>
