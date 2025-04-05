0dbaafd709d9d85e6b6500d1de10414891dbff8d
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-05 20:16:50 +0900

    🎨 [SCRUM-83]-fe-완료-button-컴포넌트-수정

--- components/common/ActionButton.tsx
+++ components/common/ActionButton.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useState, ReactNode } from 'react';
 
 type Props = {
   onClick: () => void;
+  icon?: ReactNode;
+  children: ReactNode;
 };
 
-export default function CompleteButton({ onClick }: Props) {
+export const ActionButton = ({ icon, onClick, children }: Props) => {
   const [isCompleted, setIsCompleted] = useState(false);
 
   const handleClick = () => {
@@ -21,14 +23,12 @@ export default function CompleteButton({ onClick }: Props) {
       className={`
         bg-transparent hover:bg-transparent shadow-none border-none px-0 py-0 h-auto
         flex items-center gap-1 text-sm font-bold
-        ${isCompleted ? 'text-[#744D2C]' : 'text-[#7A4A1D]'}
-        hover:text-[#A35F30] transition-colors duration-200
+        ${isCompleted ? 'text-[#744D2C]' : 'text-[#744D2C]'}
+        hover:text-[#FDA63A] transition-colors duration-200
       `}
     >
-      완료
+      {icon && <span className="mr-1">{icon}</span>}
+      {children}
     </Button>
   );
-}
-
-// 다른 파일에서 사용시 아래 코드 입력
-//  <CompleteButton onClick={() => console.log('클릭됨!')} />
+};
--- components/service/home/Home.tsx
+++ components/service/home/Home.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { Logo } from '@/components/common/Logo';
 import { Header } from '@/components/shared/Header';
 import Card from '@/components/common/Card';
 import { ProgressBar } from '@/components/service/home/ProgressBar';
-import CompleteButton from '@/components/common/CompleteButton';
+import { ActionButton } from '@/components/common/ActionButton';
+import { Plus } from 'lucide-react';
 
 export const Home = () => {
   return (
@@ -14,6 +16,12 @@ export const Home = () => {
       <div className="mt-6 flex flex-col items-center gap-4">
         <Card title="목표 진행률" isMore>
           <ProgressBar value={75} title="영어" />
+          {/* <ActionButton onClick={() => console.log('추가')} icon={<Plus size={16} />}>
+            추가
+          </ActionButton>
+          <ActionButton onClick={() => console.log('완료')}>완료</ActionButton>
+
+          <ActionButton onClick={() => console.log('삭제')}>삭제</ActionButton> */}
         </Card>
       </div>
     </div>
