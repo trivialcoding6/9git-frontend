1eace475f569e27141016d9c9a98efb1288e1604
Yoonsoyoung02 <yoonsoyoung56@gmail.com>
2025-04-13 22:56:10 +0900

    코드 리뷰 반영

--- components/common/ActionButton.tsx
+++ components/common/ActionButton.tsx
@@ -11,15 +11,7 @@ type Props = {
   className?: string;
 };
 
-export const ActionButton = ({
-  icon,
-  onClick,
-  children,
-  type = 'button',
-  className = `bg-transparent hover:bg-transparent shadow-none border-none px-0 py-0 h-auto
-        flex items-center gap-1 text-sm font-bold text-secondary
-        hover:text-primary transition-colors duration-200`,
-}: Props) => {
+export const ActionButton = ({ icon, onClick, children, type = 'button', className }: Props) => {
   const [isCompleted, setIsCompleted] = useState(false);
 
   const handleClick = () => {
@@ -27,8 +19,17 @@ export const ActionButton = ({
     onClick?.();
   };
 
+  const defaultClassName = `bg-transparent hover:bg-transparent shadow-none border-none px-0 py-0 h-auto
+    flex items-center gap-1 text-sm font-bold text-secondary
+    hover:text-primary transition-colors duration-200`;
+
   return (
-    <Button type={type} onClick={handleClick} variant="ghost" className={className}>
+    <Button
+      type={type}
+      onClick={handleClick}
+      variant="ghost"
+      className={className ?? defaultClassName}
+    >
       {icon && <div>{icon}</div>}
       {children}
     </Button>
