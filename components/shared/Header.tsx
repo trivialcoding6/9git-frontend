dd1cc8abcfc598668bbcde734cd57bb9ed16efd7
Kristyn Yoon <rhombic2@naver.com>
2025-04-16 09:31:56 +0900

    충돌 해결

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,11 +6,7 @@ export const Header = ({
   className?: string;
 }) => {
   return (
-<<<<<<< HEAD
     <header className={`sticky top-0 left-0 right-0 z-10 bg-beige-base p-1 border-b ${className}`}>
-=======
-    <header className="sticky top-0 left-0 right-0 z-10 bg-beige-base p-1 border-b">
->>>>>>> d1b8422 (✨ [SCRUM-32] FE 프로필 마크업)
       {children}
     </header>
   );
